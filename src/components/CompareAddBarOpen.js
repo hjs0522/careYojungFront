ee7e40db0fbd6126a59faf7f61197d614d1d3802
hjs0522 <91768822+hjs0522@users.noreply.github.com>
2022-08-21 11:20:44 +0900

    #compareItem remove 구현

--- src/components/CompareAddBarOpen.js
+++ src/components/CompareAddBarOpen.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button, Container, Icon} from "semantic-ui-react";
 import styled from "styled-components";
+import CompareList from "./CompareList";
 
 const CompareAddBarContainer = styled.div`
     display: flex;
@@ -12,18 +13,8 @@ const CompareAddBarContainer = styled.div`
     border-top: solid;
 `
 
-const WishItemContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    & >div{
-        padding: 20px 50px;
-        margin: 10px;
-        border: dotted;
-        border-radius: 10%;
-    }
-`
-const WishItem = styled.div`
+
+const CompareItem = styled.div`
     background-color: #496ace;
     border-radius: 10%;
     border: solid;
@@ -46,22 +37,6 @@ const TextContainer = styled.div`
 
 const CompareAddBarOpen = ({toggleIsBarOpen,compareList,onRemove})=>  {
     
-    const handleOnRemove = (id) =>{
-        onRemove(id);
-    }
-    
-    const getCompare = ()=> {
-        const result=[]
-        for(let i=0;i<3;i++){
-          if (compareList.length < i+1){
-            result.push(<div><Icon name="plus"></Icon></div>);
-          }
-          else{
-            result.push(<WishItem>{compareList[i].name}<Icon name="remove circle" color="white" onClick={handleOnRemove}></Icon></WishItem>)
-          }
-        }
-        return result;
-    }
     
     
     return(
@@ -70,9 +45,7 @@ const CompareAddBarOpen = ({toggleIsBarOpen,compareList,onRemove})=>  {
             <h2>시설비교</h2>
             <p>비교할 시설을 선택해주세요.</p>
         </TextContainer>
-        <WishItemContainer>
-            {getCompare()}
-        </WishItemContainer>
+        <CompareList compareList={compareList} onRemove = {onRemove}></CompareList>
         <ButtonContainer>
             <Button>비교하기</Button>
             <Button onClick={toggleIsBarOpen}>닫기<Icon name="angle down"></Icon></Button>
