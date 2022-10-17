c282493614b71fd9bc2abc25cfa1d0769bf52f1e
fdsa20040200 <fdsa200@naver.com>
2022-10-17 15:45:09 +0900

    [FE] 지도 테스트 연결 성공

--- src/components/Map.js
+++ src/components/Map.js
@@ -37,23 +37,23 @@ function Map({ list, mapArr }) {
           // 마커에 커서가 오버됐을 때 마커 위에 표시할 인포윈도우를 생성합니다
           var bbbbb = document.getElementById("maplist" + item.nursingHome_id);
           // 마커에 마우스오버 이벤트를 등록합니다
-          kakao.maps.event.addListener(
-            wholemap,
-            "click",
-            function (mouseEvent) {
-              // 클릭한 위도, 경도 정보를 가져옵니다
-              var latlng = mouseEvent.latLng;
+          // kakao.maps.event.addListener(
+          //   wholemap,
+          //   "click",
+          //   function (mouseEvent) {
+          //     // 클릭한 위도, 경도 정보를 가져옵니다
+          //     var latlng = mouseEvent.latLng;
 
-              // 마커 위치를 클릭한 위치로 옮깁니다
-              marker.setPosition(latlng);
+          //     // 마커 위치를 클릭한 위치로 옮깁니다
+          //     marker.setPosition(latlng);
 
-              var message =
-                "클릭한 위치의 위도는 " + latlng.getLat() + " 이고, ";
-              message += "경도는 " + latlng.getLng() + " 입니다";
+          //     var message =
+          //       "클릭한 위치의 위도는 " + latlng.getLat() + " 이고, ";
+          //     message += "경도는 " + latlng.getLng() + " 입니다";
 
-              console.log(message);
-            }
-          );
+          //     console.log(message);
+          //   }
+          // );
           kakao.maps.event.addListener(marker, "mouseover", () => {
             // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
             bbbbb.style.backgroundColor = "#97b1ff";
@@ -81,15 +81,59 @@ function Map({ list, mapArr }) {
     });
     //마커의 위치를 움직일 때마다 현재 보고있는 지도 화면의 위치 얻기
     kakao.maps.event.addListener(wholemap, "mouseup", () => {
-      console.log(wholemap.getCenter()["La"]);
-      console.log(wholemap.getCenter()["Ma"]);
+      // console.log(wholemap.getCenter()["La"]);
+      // console.log(wholemap.getCenter()["Ma"]);
       const currentMap = {
-        LB1: wholemap.getCenter()["La"] - 0.12189182448,
-        LB2: wholemap.getCenter()["Ma"] - 0.0603703707,
-        RT1: wholemap.getCenter()["La"] + 0.12189182448,
-        RT2: wholemap.getCenter()["Ma"] + 0.0603703707,
+        centerX: wholemap.getCenter()["La"],
+        centerY: wholemap.getCenter()["Ma"],
+        grade: 12,
+        keyword: "aaa",
+        order: "aaa",
+        memberId: "user1",
+        leftX: wholemap.getCenter()["La"] - 0.12189182448,
+        leftY: wholemap.getCenter()["Ma"] - 0.0603703707,
+        rightX: wholemap.getCenter()["La"] + 0.12189182448,
+        rightY: wholemap.getCenter()["Ma"] + 0.0603703707,
+        service: "asd",
       };
-      console.log(currentMap);
+      // let query = Object.keys(currentMap)
+      //   .map(
+      //     (k) => encodeURIComponent(k) + "=" + encodeURIComponent(currentMap[k])
+      //   )ㅜ
+      //   .join("&");
+      // const url = "https://83e5-14-40-73-49.jp.ngrok.io/search/map?" + query;
+      const url =
+        "https://ca4d-14-40-73-49.jp.ngrok.io/search/map?centerX=127.02080081879996&centerY=37.47233610357272&grade=12&keyword=aaa&order=aaa&memberId=user1&leftX=126.89890899431995&leftY=37.41196573287272&rightX=127.14269264327996&rightY=37.53270647427272&service=asd";
+      fetch(url, {
+        method: "GET",
+        headers: {
+          "ngrok-skip-browser-warning": "69420",
+          accept: "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          console.log(res);
+        });
+      // fetch(url, {
+      //   headers: {
+      //     accept: "application/json",
+      //   },
+      // })
+      //   .then((res) => {
+      //     if (res.ok === true) {
+      //       return console.log(res);
+      //     }
+      //     throw new Error("에러!");
+      //   })
+      //   .catch((err) => {
+      //     alert(err);
+      //   })
+      //   .then((data) => {
+      //     console.log(data);
+      //   });
+
+      // console.log(currentMap);
     });
   }, []);
 
