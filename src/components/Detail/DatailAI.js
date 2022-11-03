d412d0cf63613c699e09ce2f0b7cc54f58b451a8
fdsa20040200 <fdsa200@naver.com>
2022-11-03 14:13:17 +0900

    상세보기 페이지 ai 컴포넌트 변경

--- src/components/Detail/DatailAI.js
+++ src/components/Detail/DatailAI.js
@@ -1,7 +1,15 @@
 import styled from "styled-components";
+import { Icon, Popup } from "semantic-ui-react";
 
 const DetailTitle = styled.h2({
   fontWeight: "normal",
+  fontSize: "24px",
+  display: "inline-block",
+});
+
+const StyledPopup = styled.div({
+  display: "inline-block",
+  marginLeft: "5px",
   fontSize: "24px",
 });
 
@@ -35,24 +43,35 @@ const DetailAIGraph = styled.div({
 
   borderRadius: "15px",
 });
+// #a3a1fd
 const DetailAIInnerGraph = styled.div`
   position: absolute;
   display: inline-block;
   height: 10px;
   background: linear-gradient(270deg, #496ace 0%, #a3a1fd 100%);
   border-radius: 15px;
-  width: ${(prop) => prop.width || "20%"};
+  width: ${(prop) => (prop.width * 100) / 3.8 + "%" || "20%"};
 `;
 const AIarr = [
-  { name: "긍정도", value: "20%" },
-  { name: "부정도", value: "10%" },
-  { name: "혼합도", value: "8%" },
+  { name: "긍정도", value: 0.8 },
+  { name: "부정도", value: 0.1 },
+  { name: "혼합도", value: 0.08 },
 ];
 
 function DetailAI() {
   return (
     <StyledDetailBox id="Detail-4">
       <DetailTitle>AI점수</DetailTitle>
+      <StyledPopup>
+        <Popup
+          content={
+            "케어요정의 리뷰와 여러 인터넷상의 소셜 데이터를 종합, 분석하여 각 시설의 평가를 나타냅니다."
+          }
+          key={"explain"}
+          header={"AI점수란?"}
+          trigger={<Icon name="question" size="small" color="red" />}
+        />
+      </StyledPopup>
       <StyledDetailAI>
         {AIarr.map((i) => (
           <DetailAIElement>
@@ -63,7 +82,7 @@ function DetailAI() {
             <DetailAIText
               style={{ width: "0", color: "#496ace", marginLeft: "3%" }}
             >
-              {i.value}
+              {i.value * 100}%
             </DetailAIText>
           </DetailAIElement>
         ))}
