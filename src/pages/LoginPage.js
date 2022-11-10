e8413ccf4ef763f19e5aa318d71e4fb24c105c50
fdsa20040200 <fdsa200@naver.com>
2022-11-10 15:46:53 +0900

    [fe] 카카오 로그인 이미지 추가

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,52 +1,52 @@
-import { Button} from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 import styled from "styled-components";
 import { KAKAO_AUTH_URL } from "../OAuth";
+import kakaoimg from "../img/kakaoPng.png";
 
 const PageContainer = styled.div`
-    background-color: #F5F7FA;
-    height: 80vh;
-    padding-top: 10vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+  background-color: #f5f7fa;
+  height: 80vh;
+  padding-top: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const LoginButtonContainer = styled.div`
-    background-color: white;
-    width: 35vw;
-    height: 35vh;
-    border-radius: 5px;
+  background-color: white;
+  width: 35vw;
+  height: 35vh;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  padding-bottom: 5vh;
+  & div {
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    justify-content: space-around;
-    padding-bottom: 5vh;
-    & div{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-    }
-    
-`
+  }
+`;
 
+const LoginPage = () => {
+  console.log(KAKAO_AUTH_URL);
 
-const LoginPage = ()=>{
-    console.log(KAKAO_AUTH_URL)
-    
-    return(
-        <PageContainer>
-            <LoginButtonContainer>
-                <div>
-                    <h3>간편 로그인</h3>
-                    <text>쉽고 빠르게 케어요정을 시작해보세요!</text>
-                </div>
-                <a href={KAKAO_AUTH_URL}>카카오</a>
-                <img src={process.env.PUBLIC_URL+'/img/kakaoPng.png'} alt="kakao"></img>
-                <Button>b</Button>
-                <Button>c</Button>
-            </LoginButtonContainer>
-        </PageContainer>
-    );
-}
+  return (
+    <PageContainer>
+      <LoginButtonContainer>
+        <div>
+          <h3>간편 로그인</h3>
+          <text>쉽고 빠르게 케어요정을 시작해보세요!</text>
+        </div>
+        <a href={KAKAO_AUTH_URL}>
+          <img src={kakaoimg} alt="kakao"></img>
+        </a>
+        <Button>b</Button>
+        <Button>c</Button>
+      </LoginButtonContainer>
+    </PageContainer>
+  );
+};
 export default LoginPage;
